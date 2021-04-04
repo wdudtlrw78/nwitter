ab21e73abfb8c31f814c4d551178ae650012cd33
momo <wdudtlrw78@gmail.com>
2021-04-05 01:58:30 +0900

    Updated creating account

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,6 +1,49 @@
-import React from 'react';
-import fbase from 'fbase';
+import { authService } from 'fbase';
+import React, { useState } from 'react';
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [newAccount, setNewAccount] = useState(true);
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        // create account
+        data = await authService.createUserWithEmailAndPassword(email, password);
+      } else {
+        // log in
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
+        <input name="password" type="password" placeholder="password" required value={password} onChange={onChange} />
+        <input type="submit" value={newAccount ? 'Create Account' : 'Log In'} />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
