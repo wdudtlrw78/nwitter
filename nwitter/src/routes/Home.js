1f3b5a40f1b1be9d5350b75ffaa20e0243b47873
momo <wdudtlrw78@gmail.com>
2021-04-05 15:26:48 +0900

    Updated Form and Database Setup

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState('');
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind" maxLength={120} />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
