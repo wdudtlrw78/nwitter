41e17ee77b0f0849b66c505a215c4a8a149c0843
momo <wdudtlrw78@gmail.com>
2021-04-06 13:33:09 +0900

    Updated Preview Images

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -4,15 +4,12 @@ import { authService } from 'fbase';
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser); //authService.currentUser 이 부분에서 로드될 때 너무 빠르게 흘러가서 실제로 로그인인지 아닌지 판단할 수 없다
-  // 그래서 항상 로그아웃이 된다. 바로 즉시 왜냐하면 firebase가 초기화되고 모든 걸 로드할 때까지 기다려줄 시간이 없기때문
   const [userObj, setUserObj] = useState(null);
 
   useEffect(() => {
     // 실제로 유저들이 로그인이 되었는지 안 되었는지 알 수있다.
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true);
         setUserObj(user);
       } else {
         setIsLoggedIn(false);
@@ -23,7 +20,7 @@ function App() {
 
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : 'Initializing...'}
+      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} /> : 'Initializing...'}
       <footer>&copy; {new Date().getFullYear()}</footer>
     </>
   );
--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -1,9 +1,11 @@
+import Nweet from 'components/Nweet';
 import { dbService } from 'fbase';
 import React, { useEffect, useState } from 'react';
 
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState('');
   const [nweets, setNweets] = useState([]);
+  const [attachment, setAttachment] = useState();
   useEffect(() => {
     dbService.collection('nweets').onSnapshot((snapshot) => {
       const nweetArray = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
@@ -25,17 +27,39 @@ const Home = ({ userObj }) => {
     } = e;
     setNweet(value);
   };
+
+  const onFileChange = (e) => {
+    const {
+      target: { files },
+    } = e;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+
+  const onClearAttachment = () => setAttachment(null);
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind" maxLength={120} />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Nweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} width="50px" height="50px" />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {nweets.map((nweet) => (
-          <div key={nweet.id}>
-            <h4>{nweet.text}</h4>
-          </div>
+          <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.createId === userObj.uid} />
         ))}
       </div>
     </div>
